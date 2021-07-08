c4d55805bd4b53c7ff12156a377557991e876bd1
Striver <1312395217@qq.com>
2021-07-08 22:24:27 +0800

    Add all the job code

--- src/main/java/com/bas/processing/SpringBatchProcessingApplication.java
+++ src/main/java/com/bas/processing/SpringBatchProcessingApplication.java
@@ -5,20 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 
 
-/**
- * @SpringBootApplication 做了如下事情：
- * 
- * {
- * 1.@Configuration：将类标记为应用程序上下文的Bean定义的源。
- * 
- * 2.@EnableAutoConfiguration：告诉Spring Boot根据类路径设置，其他bean和各种属性设置开始添加bean。
- * 例如，如果spring-webmvc在类路径上，则此注释将应用程序标记为Web应用程序并激活关键行为，例如设置DispatcherServlet。
- * 
- * 3.@ComponentScan：告诉Spring在包中寻找其他组件，配置和服务com/lb/processing，让它找到控制器。
- * }
- * @author bas
- *
- */
 @SpringBootApplication
 public class SpringBatchProcessingApplication {
 
